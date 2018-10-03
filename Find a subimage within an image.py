111f187ebd078889a592b17c75a74cb3c6bb0e57
Muthu Krishnan <muthukrishnan749@gmail.com>
2018-10-03 07:24:48 +0530

    some misc learnings

--- Find a subimage within an image.py
+++ Find a subimage within an image.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[5]:
+# In[ ]:
 
 
 # from skimage.feature import match_template
@@ -10,21 +10,21 @@
 # sub_image = imread('resources/smaller.jpg')
 # template = imread('resources/puppy.jpg')
 
-import cv2
-from cv2 import cv
+import cv2 as cv
 import matplotlib.pyplot as plt
 
-method = cv.CV_TM_SQDIFF_NORMED
+method = cv.TM_CCOEFF_NORMED
 
 # Read the images from the file
-#small_image = cv2.imread('resources/smaller.jpg')
-small_image = cv2.imread('resources/sub_font.png')
-large_image = cv2.imread('resources/biggerimage.png')
+small_image = cv.imread('resources/smaller.png')
+small_image = cv.imread('resources/boxsf.png')
+large_image = cv.imread('resources/biggerimage.png')
+large_image = cv.imread('resources/sf3.png')
 
-result = cv2.matchTemplate(small_image, large_image, method)
+result = cv.matchTemplate(small_image, large_image, method)
 
 # We want the minimum squared difference
-mn,_,mnLoc,_ = cv2.minMaxLoc(result)
+mn,_,mnLoc,_ = cv.minMaxLoc(result)
 
 # Draw the rectangle:
 # Extract the coordinates of our best match
@@ -34,9 +34,10 @@ MPx,MPy = mnLoc
 trows,tcols = small_image.shape[:2]
 
 # Step 3: Draw the rectangle on large_image
-cv2.rectangle(large_image, (MPx,MPy),(MPx+tcols,MPy+trows),(0,0,255),2)
+cv.rectangle(large_image, (MPx,MPy),(MPx+tcols,MPy+trows),(0,0,255),2)
 
 # Display the original image with the rectangle around the match.
-plt.imshow(large_image)
-plt.show()
+# cv.imshow('output',large_image)
+cv.imwrite('output.png',large_image)
+# plt.imshow(large_image)
 
